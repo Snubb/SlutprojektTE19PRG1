7e379d7659d4d5ce46cc90935701fcfcd43041ef
Snubb <60389050+Snubb@users.noreply.github.com>
2021-04-27 11:02:51 +0200

    Tog bort en liten justering

--- Dokumentation/info.md
+++ Dokumentation/info.md
@@ -1,1 +1,15 @@
 Här sprar du alla dokumentationsfiler. Skisser på Gui, grafik, flödesdiagram
+
+Idé:
+En form av singleplayer version av battleship.
+![Battleship_example](https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Battleship_game_board.svg/800px-Battleship_game_board.svg.png)
+(bilden tagen från wikipedia)  
+Spelet går ut på att helt enkelt skjuta ner alla skepp.  
+Skeppen är från början osynliga och delar av ett skepp blir synligt när man träffar den.  
+Spelet är gjort i en grid och man har en specifik mängd med skott innan man förlorar.  
+På kanten får man se mängden skepp kvar, hur stora/långa dom är, och ifall man sänkt dom.  
+
+Planering:
+Börja med att skapa själva griden som man spelar på. Kanske dela in allting i rektanglar som båtarna ligger i.  
+Tanken är att själva griden bara är en stor fyrkant sedan har jag en lista med alla individuella fyrkanter.  
+När man "skjuter" på en fyrkant kommer spelet kolla musens nuvarande position och kolla om en båt ligger i det området.  
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,113 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.beans.FeatureDescriptor;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.ArrayList;
+
 /**
  * Created 2021-04-27
  *
  * @author
  */
-public class Main {
+public class Main extends Canvas implements Runnable{
+    private final int width = 400; //Dimensions for playing area
+    private final int height = 600;
+
+    private boolean isRunning;
+
+    private Thread thread;
+
+    int fps = 120;
+
+    private BufferStrategy bs;
+
+    public Main() {
+        JFrame frame = new JFrame("Battleship clone");
+        this.setSize(width,height);
+        frame.add(this);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(new KL());
+        frame.setVisible(true);
+
+        isRunning = false;
+    }
+
+    public void update() {
+
+    }
+
+    public void draw() {
+        bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+
+        g.dispose();
+        bs.show();
+    }
+
     public static void main(String[] args) {
         // Här startas ditt program
+        Main painting = new Main();
+        painting.start();
     }
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        double deltaT = 1000.0/fps;
+        long lastTime = System.currentTimeMillis();
+
+        while (isRunning) {
+            long now = System.currentTimeMillis();
+            if (now-lastTime > deltaT) {
+                update();
+                draw();
+                lastTime = now;
+            }
+        }
+        stop();
+    }
+
+    private class KL implements KeyListener {
+        @Override
+        public void keyTyped(KeyEvent keyEvent) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent keyEvent) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent keyEvent) {
+
+        }
+    }
+
 }
